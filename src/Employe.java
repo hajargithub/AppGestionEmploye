dcc9bddc891c1b8acb084a3476b322a2ee005aa0
HARKAOUI Hajar <hajar15harkaoui@gmail.com>
2025-01-22 09:47:52 +0100

    Update Employe.java

--- src/Employe.java
+++ src/Employe.java
@@ -17,14 +17,37 @@ public class Employe {
     }
 
     // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-    public String getPoste() { return poste; }
-    public void setPoste(String poste) { this.poste = poste; }
-    public double getSalaire() { return salaire; }
-    public void setSalaire(double salaire) { this.salaire = salaire; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPoste() {
+        return poste;
+    }
+
+    public void setPoste(String poste) {
+        this.poste = poste;
+    }
+
+    public double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(double salaire) {
+        this.salaire = salaire;
+    }
 
     // toString method
     @Override
